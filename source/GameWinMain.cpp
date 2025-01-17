a04062506da700e70c53790a44569caa2618f617
Juddy2403 <ioanaraileanu24@yahoo.com>
2025-01-17 22:08:32 +0100

    feat: ran simple lua script

--- source/GameWinMain.cpp
+++ source/GameWinMain.cpp
@@ -9,7 +9,8 @@
 #include "GameWinMain.h"
 #include "GameEngine.h"
 
-#include "Game.h"	
+#include "Game.h"
+#include "LuaBindings.h"
 
 //-----------------------------------------------------------------
 // Create GAME_ENGINE global (singleton) object and pointer
@@ -17,11 +18,41 @@
 GameEngine myGameEngine;
 GameEngine* GAME_ENGINE{ &myGameEngine };
 
+// Creating a second window for the console (debugging purposes)
+void AllocateConsole()
+{
+	if (AllocConsole())                          // Allocate a new console for the application
+	{
+		FILE *fp;                                // Redirect STDOUT to the console
+		freopen_s(&fp, "CONOUT$", "w", stdout);
+		setvbuf(stdout, NULL, _IONBF, 0);        // Disable buffering for stdout
+
+		freopen_s(&fp, "CONOUT$", "w", stderr);  // Redirect STDERR to the console
+		setvbuf(stderr, NULL, _IONBF, 0);        // Disable buffering for stderr
+
+		freopen_s(&fp, "CONIN$", "r", stdin);    // Redirect STDIN to the console
+		setvbuf(stdin, NULL, _IONBF, 0);         // Disable buffering for stdin
+
+		std::ios::sync_with_stdio(true);         // Sync C++ streams with the console
+	}
+}
+
 //-----------------------------------------------------------------
 // Main Function
 //-----------------------------------------------------------------
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
+	AllocateConsole();
+	sol::state lua;
+	lua.set_exception_handler(&LuaBindings::ExceptionsHandler);
+	//luaL_openlibs(lua.lua_state());
+	lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::math, sol::lib::table, sol::lib::string);
+
+	LuaBindings::RegisterBindings(lua);
+
+	// Load and execute your Lua scripts here if needed
+	lua.script_file("luaFiles/script.lua");
+
 	GAME_ENGINE->SetGame(new Game());					// any class that implements AbstractGame
 
 	return GAME_ENGINE->Run(hInstance, nCmdShow);		// here we go
---
+++ source/LuaBindings.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <sol/optional_implementation.hpp>
+#include <sol/state.hpp>
+#include <sol/string_view.hpp>
+#include "lua.h"
+
+class LuaBindings {
+public:
+    static void RegisterBindings(sol::state& lua);
+    static int ExceptionsHandler(lua_State* L, sol::optional<const std::exception&> maybe_exception, sol::string_view description);
+};
